0ea9ea2ff603f1899aa990e8e19f5933dfe3b535
MarcoMoro10 <marco.morelli15@studio.unibo.it>
2025-02-24 11:39:24 +0100

    add testSetPin test

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -5,8 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+    private static boolean lockState = false;
     @Test
     public void todo() {
         assertTrue(true);
     }
+
+    @Test
+    public void testSetPin() {
+        assertFalse(lockState);
+    }
+
 }
