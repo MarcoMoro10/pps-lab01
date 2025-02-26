834e9660b3f7e69702f9b1163fa4b93b788b3d6f
MarcoMoro10 <marco.morelli15@studio.unibo.it>
2025-02-26 10:12:25 +0100

    new Class MinxMaxStackImpl

--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -1,12 +1,20 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
+    private MinMaxStackImpl minMaxStack;
+    @BeforeEach
+    void setUp(){
+        minMaxStack = new MinMaxStackImpl();
+    }
     @Test
     public void todo() {
         assertTrue(true);
     }
+
+
 }
