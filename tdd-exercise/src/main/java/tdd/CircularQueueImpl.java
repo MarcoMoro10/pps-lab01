11428557747e675f2cda7d5a85cc06a66ab650a1
MarcoMoro10 <marco.morelli15@studio.unibo.it>
2025-02-26 22:55:53 +0100

    new Enqueue test

---
+++ tdd-exercise/src/main/java/tdd/CircularQueueImpl.java
@@ -0,0 +1,59 @@
+package tdd;
+
+
+public class CircularQueueImpl implements CircularQueue {
+    private static final int DEFAULT_CAPACITY = 3;
+    private int[] queue;
+    private int size;
+    private int head;
+    private int tail;
+    public CircularQueueImpl() {
+        queue = new int[DEFAULT_CAPACITY];
+        this.size = 0;
+        this.head = 0;
+        this.tail = 0;
+    }
+    @Override
+    public void enqueue(int value) {
+        if(isFull()){
+            head = (head +1) % DEFAULT_CAPACITY;
+        }else{
+            size++;
+        }
+        queue[tail] = value;
+        tail = (tail + 1) % DEFAULT_CAPACITY;
+    }
+
+    @Override
+    public int dequeue() {
+        return 0;
+    }
+
+    @Override
+    public int peek() {
+        if(isEmpty()){
+            return -1;
+        }
+        return queue[head];
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isFull() {
+        return size == DEFAULT_CAPACITY;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int capacity() {
+        return DEFAULT_CAPACITY;
+    }
+}
--- tdd-exercise/src/test/java/tdd/CircularListTest.java
+++ tdd-exercise/src/test/java/tdd/CircularListTest.java
@@ -1,6 +1,7 @@
 package tdd;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,8 +10,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
+    private CircularQueueImpl circularQueue;
+    private static int firstValue = 5;
+    @BeforeEach
+    void setUp() {
+        circularQueue = new CircularQueueImpl();
+    }
     @Test
     public void todo() {
         assertTrue(true);
+    }
+    @Test
+    public void testEnqueue() {
+        circularQueue.enqueue(firstValue);
+        assertEquals(5, circularQueue.peek());
     }
 }
